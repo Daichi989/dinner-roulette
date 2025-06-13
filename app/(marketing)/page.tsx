14b12bd742f39f4f55891126bb4bfa68cc53f4eb
Daichi <mochimochi989@gmail.com>
2025-06-14 08:41:31 +0900

    アプリ名を「晩ごはんどうする？」から「DinnerRoulette」に変更し、マーケティングページのタイトルを画像に置き換えました

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
     <>
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12 min-h-screen">
         <div className="flex flex-col items-center gap-4 mt-16">
-            <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">晩ごはんどうする？</h1>
-            <p className="text-muted-foreground">今日の晩ごはんはこれで決まり！</p>
+            <img src="夜ご飯.png" className="md:w-1/2"/>
+            <p className="text-muted-foreground">今日はこれで決まり！</p>
             <div className="flex gap-4">
                 <Link href="/auth/signup" className={cn(buttonVariants({ size: "lg" }),"bg-orange-400")}>
                     はじめる
