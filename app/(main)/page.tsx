4aa6ffac889f9f86ea00f0abb812c415a4d5ee8e
Daichi <mochimochi989@gmail.com>
2025-06-17 13:18:47 +0900

    ユーザー認証機能を追加し、GitHubをプロバイダーとして設定しました。また、関連する依存関係を更新しました。

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <>
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12 min-h-screen">
         <div className="flex flex-col items-center gap-4 mt-16">
-            <img src="夜ご飯.png" className="md:w-1/2 animate-in fade-in-0 slide-in-from-top-8 "/>
+            <img src="夜ご飯.png" className="md:w-1/2 animate-in fade-in-0 slide-in-from-top-8 duration-1500"/>
             <p className="text-muted-foreground">そんな時はルーレットで決めちゃおう！</p>
             <div className="flex gap-4">
                 <Link href="/register" className={cn(buttonVariants({ size: "lg" }),"bg-orange-400")}>
