dbdb9414329226e0b9067afdcda20c5732a08e2d
Daichi <mochimochi989@gmail.com>
2025-06-17 22:51:42 +0900

    サイトフッターのスタイルを修正し、画像のクラスを追加

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,9 +1,9 @@
 export default function SiteFooter() {
     return (
-        <footer className="py-4" >
+        <footer className="" >
           <section className="flex flex-col items-center bg-yellow-200">
             <p className="text-muted-foreground">developed by Daichi</p>
-            <img src="line_vegetable.png"/>
+            <img className="w-full max-w-8xl mx-auto" src="line_vegetable.png"/>
          </section>
         </footer>
     );
