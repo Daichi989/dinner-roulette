d6a184feba0eae7b5531f9431f7614c99172a434
Daichi <mochimochi989@gmail.com>
2025-06-20 22:27:03 +0900

    ログアウトボタンを追加し、サインアウト機能を実装

--- app/roulette/page.tsx
+++ app/roulette/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+
 import MainNav from "@/components/main-nav";
 import CookingRoulette from "@/components/roulette";
 import { buttonVariants } from "@/components/ui/button";
 import { marketingConfig } from "@/config/marketing";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 
 export default function Page(){
@@ -12,8 +14,9 @@ export default function Page(){
         <div className="h-16 px-4 flex items-center justify-between">
           <MainNav items={marketingConfig.mainNav} />
           <nav className="flex items-center gap-4">
-            <Link href={"/login"} className={cn(buttonVariants({variant:"secondary", size: "sm"}),"px-4")} >アイコン</Link>
-            <Link href={"/roulette"} className={cn(buttonVariants({size: "sm"}),"px-4 bg-orange-400")} >＋レシピを書く</Link>
+            <button className={cn(buttonVariants({variant:"secondary", size: "sm"}),"px-4")}
+                    onClick={()=>{signOut({callbackUrl: "/"})}}
+             >ログアウト</button>
           </nav>
         </div>
     <CookingRoulette />
